7c762e18ecadd1396378d3d52a58eeb5646a0948
Thomas Bilbault <thomas.bilbault@gmail.com>
2023-03-14 14:38:29 +0100

    récupération de donnée de l'api

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,22 +1,34 @@
-// fetch('http://localhost:5678/api/works')
-//   .then(result => result.json())
-//   .then(data => console.log(data));
 
-const projetData = fetch('http://localhost:5678/api/works').then(projetData => projetData.json());
-console.log(projetData)
 
-  
- function projectList(projetData){
-     for (let i = 0; i < projetData.length; i++){
-         const projet = projetData[i]; 
-         
-         const divGallery = document.querySelector('.gallery');
-         const dataElement = document.createElement("figure");
-        
-     }
- }
+const url = 'http://localhost:5678/api/works';
+
+fetch(url)
+.then((response) => response.json())
+.then((data) => {
+
+    function projectList(data){
+        for (let i = 0; i < data.length; i++){
+   
+            const projet = data[i]; 
+            
+            const divGallery = document.querySelector('.gallery');
+            const dataElement = document.createElement("figure");
+            dataElement.dataset.id = data[i].id
+            const imgElement = document.createElement("img");
+            imgElement.src = projet.imageUrl;
+            const titleElement = document.createElement("figcaption");
+            titleElement.innerText = projet.title;
+   
+            divGallery.appendChild(dataElement);
+            dataElement.appendChild(imgElement);
+            dataElement.appendChild(titleElement);
+           
+        }
+    }
+    projectList(data)
+});
+
  
- projectList(projetData)
 
 
 
