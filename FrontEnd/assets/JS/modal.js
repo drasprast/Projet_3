5647a1355d2f88f403cc22b44ccdfa33e304de2c
Thomas Bilbault <thomas.bilbault@gmail.com>
2023-04-27 10:00:22 +0200

    commit

--- FrontEnd/assets/JS/modal.js
+++ FrontEnd/assets/JS/modal.js
@@ -1,54 +1,51 @@
 let modal = null;
-// const urlModal = 'http://localhost:5678/api/works';
-// let projectsDataModal;
-// const divGalleryModal = document.querySelector('.gallery_modal');
+
+const urlModal = 'http://localhost:5678/api/works';
+let projectsDataModal;
+const divGalleryModal = document.querySelector('.gallery_modal');
 
 if (localStorage.token) {
-  const filtres = document.querySelector('.filtres');
-  filtres.style.display = "none";
-
-  const lienModifier = document.querySelector('.lien_modifier');
-  const aElement = document.createElement("a");
-  const iElement = document.createElement("i");
-  aElement.innerText = "modifier";
-  aElement.setAttribute("href", "#modal1");
-  aElement.classList.add("js_modal", "filtres");
-  iElement.classList.add("fa-solid", "fa-pen-to-square")
-
-  const logout = document.querySelector('.js_logout');
-  logout.innerText = "";
-  const aElementLogout = document.createElement("button");
-  aElementLogout.innerText = "logout";
-  aElementLogout.classList.add("js_button_logout");
-  logout.appendChild(aElementLogout);
-
-  lienModifier.appendChild(iElement);
-  lienModifier.appendChild(aElement);
-
-  // fetch(urlModal)
-  // .then((response) => response.json())
-  // .then((dataModal) => {
-  //   projectsDataModal = dataModal;
-  // })
-  // .catch((error) => console.log('Une erreur est survenue:', error));
-
-  // function projectDisplay(dataModal) {
-  //   divGalleryModal.innerHTML = "";
-  //   dataModal.forEach((projet) => {
-  //     const dataElement = document.createElement("figure");
-  //     dataElement.dataset.id = projet.id;
-  //     dataElement.dataset.categoryId = projet.categoryId;
+  modeEdition();
   
-  //     const imgElement = document.createElement("img");
-  //     imgElement.src = projet.imageUrl;
 
-  //     const titleElement = document.createElement("figcaption");
-  //     titleElement.innerText = "éditer";
-  
-  //     dataElement.appendChild(imgElement);
-  //     divGalleryModal.appendChild(dataElement);
-  //   });
-  // }
+  fetch(urlModal)
+  .then((response) => response.json())
+  .then((dataModal) => {
+    projectDisplayModal(dataModal);
+  })
+  .catch((error) => console.log('Une erreur est survenue:', error));
+
+  function projectDisplayModal(dataModal) {
+    divGalleryModal.innerHTML = "";
+    dataModal.forEach((projet) => {
+      const trashIcon = document.createElement("i")
+      trashIcon.classList.add('fa-solid', 'fa-trash-can', 'trash_can')
+      trashIcon.dataset.id = projet.id;
+      const dataElement = document.createElement("figure");
+      dataElement.dataset.id = projet.id;
+      dataElement.dataset.categoryId = projet.categoryId;
+      
+      const imgElement = document.createElement("img");
+      imgElement.src = projet.imageUrl;
+
+      const titleElement = document.createElement("figcaption");
+      titleElement.innerText = "éditer";
+
+      
+      dataElement.appendChild(trashIcon);
+      dataElement.appendChild(imgElement);
+      dataElement.appendChild(titleElement);
+      divGalleryModal.appendChild(dataElement);
+
+      trashIcon.addEventListener('click', (e) =>{
+        const myKey = "token";
+        const token = localStorage.getItem(myKey);
+        const workId = e.currentTarget.dataset.id;
+        e.preventDefault();
+        deleteWork(workId, token);
+      })
+    });
+  }
 }
 
 const openModal = (e) => {
@@ -93,3 +90,72 @@ window.addEventListener('keydown', function (e) {
 });
 
 
+function modeEdition () {
+  const lienModifier = document.querySelector('.lien_modifier');
+  const logout = document.querySelector('.js_logout');
+  const blackEdition = document.querySelector('header');
+  const filtres = document.querySelector('.filtres');
+  
+  filtres.style.display = "none";
+
+  const aElement = document.createElement("a");
+  aElement.innerText = "modifier";
+  aElement.setAttribute("href", "#modal1");
+  aElement.classList.add("js_modal", "filtres");
+
+  const iElement = document.createElement("i");
+  iElement.classList.add("fa-solid", "fa-pen-to-square");
+
+  logout.innerText = "";
+  const aElementLogout = document.createElement("button");
+  aElementLogout.innerText = "logout";
+  aElementLogout.classList.add("js_button_logout");
+
+  const blackDivElement = document.createElement("div");
+  blackDivElement.classList.add("background_black_edition");
+
+  const buttonEdition = document.createElement("button");
+  const logoButtonEdition = document.createElement("i");
+  buttonEdition.classList.add("button_inherit", "button_edition");
+  logoButtonEdition.classList.add("fa-solid", "fa-pen-to-square");
+  buttonEdition.innerText = ("édition");
+
+  const buttonPublier = document.createElement("button");
+  buttonPublier.classList.add('buton_publier');
+  buttonPublier.innerText = "Publier les changements";
+
+  logout.appendChild(aElementLogout);
+  lienModifier.appendChild(iElement);
+  lienModifier.appendChild(aElement);
+  blackEdition.insertBefore(blackDivElement, blackEdition.firstChild);
+  const divBlackEdition = document.querySelector('.background_black_edition');
+  divBlackEdition.appendChild(buttonEdition);
+  buttonEdition.appendChild(logoButtonEdition);
+  divBlackEdition.appendChild(buttonPublier);
+}
+
+
+
+
+
+
+
+function deleteWork() {
+  fetch(`http://localhost:5678/api/works/${workId}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+    if (response.ok) {
+  
+      afficherImagesModal();
+     
+      afficherImages();
+    } else {
+      console.error('Erreur lors de la suppression de l\'élément');
+    }
+  });
+}
